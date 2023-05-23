2f5f6505988974855664cb4b4d7f6a29de9dd218
nansnova <a01378043@tec.mx>
2023-05-22 20:56:12 -0600

    rad/s topic publisher

--- catkin_ws/src/arduino_rpm_reader/src/rpm_reader.py
+++ catkin_ws/src/arduino_rpm_reader/src/rpm_reader.py
@@ -5,32 +5,39 @@ import serial
 from std_msgs.msg import Float32
 
 class ArduinoMotorNode:
-    def __init__(self, port, baud_rate):
+    def __init__(self, port, rate):
         rospy.init_node('rpm_reader')
-        self.serial_port = serial.Serial(port, baud_rate, timeout=1)
+        self.serial_port = serial.Serial(port, rate, timeout=1)
         self.pub_wr = rospy.Publisher('/wr', Float32, queue_size=10)
         self.pub_wl = rospy.Publisher('/wl', Float32, queue_size=10)
-        rospy.Subscriber('arduino_data', String, self.arduino_callback)
 
-    def arduino_callback(self, data):
-        velocities = data.data.split(',')
-        if len(velocities) == 2:
-            try:
-                wr = float(velocities[0])
-                wl = float(velocities[1])
-                self.pub_wr.publish(wr)
-                self.pub_wl.publish(wl)
-            except ValueError:
-                rospy.logwarn("Invalid velocity values received")
-        else:
-            rospy.logwarn("Invalid data received from Arduino")
+    def read_serial_data(self):
+        while not rospy.is_shutdown():
+            if self.serial_port.in_waiting > 0:
+                data = self.serial_port.readline().decode().rstrip('\r\n')
+                velocities = data.split(',')
+                if len(velocities) == 2:
+                    try:
+                        wr_rpm = float(velocities[0])
+                        wl_rpm = float(velocities[1])
+                        wr_rad_per_sec = self.rpm_to_rad_per_sec(wr_rpm)
+                        wl_rad_per_sec = self.rpm_to_rad_per_sec(wl_rpm)
+                        self.pub_wr.publish(wr_rad_per_sec)
+                        self.pub_wl.publish(wl_rad_per_sec)
+                    except ValueError:
+                        rospy.logwarn("Invalid velocity values received")
+                else:
+                    rospy.logwarn("Invalid data received from Arduino")
+
+    def rpm_to_rad_per_sec(self, rpm):
+        return rpm * 0.01047  # Factor de conversion de RPM a rad/s
 
     def run(self):
-        rospy.spin()
+        self.read_serial_data()
         self.serial_port.close()
 
 if __name__ == '__main__':
-    port = "/dev/ttyUSB0"
-    baud_rate = 9600
-    node = ArduinoMotorNode(port, baud_rate)
+    port = "/dev/ttyUSB0" 
+    rate = 9600 
+    node = ArduinoMotorNode(port, rate)
     node.run()
