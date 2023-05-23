e01f176fb16044755a21655152876f14e5b7a563
nansnova <a01378043@tec.mx>
2023-05-22 19:19:25 -0600

    kauil gazebo

--- catkin_ws/src/arduino_rpm_reader/src/rpm_reader.py
+++ catkin_ws/src/arduino_rpm_reader/src/rpm_reader.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import String
+import serial
+from std_msgs.msg import Float32
 
-def rpm_callback(data):
-    rpm = data.data
-    rospy.loginfo("Velocidad angular en RPM: %s", rpm)
+class ArduinoMotorNode:
+    def __init__(self, port, baud_rate):
+        rospy.init_node('rpm_reader')
+        self.serial_port = serial.Serial(port, baud_rate, timeout=1)
+        self.pub_wr = rospy.Publisher('/wr', Float32, queue_size=10)
+        self.pub_wl = rospy.Publisher('/wl', Float32, queue_size=10)
+        rospy.Subscriber('arduino_data', String, self.arduino_callback)
 
-def rpm_reader():
-    rospy.init_node('rpm_reader', anonymous=True)
-    rospy.Subscriber('rpm_topic', String, rpm_callback)
-    rospy.spin()
+    def arduino_callback(self, data):
+        velocities = data.data.split(',')
+        if len(velocities) == 2:
+            try:
+                wr = float(velocities[0])
+                wl = float(velocities[1])
+                self.pub_wr.publish(wr)
+                self.pub_wl.publish(wl)
+            except ValueError:
+                rospy.logwarn("Invalid velocity values received")
+        else:
+            rospy.logwarn("Invalid data received from Arduino")
+
+    def run(self):
+        rospy.spin()
+        self.serial_port.close()
 
 if __name__ == '__main__':
-    try:
-        rpm_reader()
-    except rospy.ROSInterruptException:
-        pass
-
+    port = "/dev/ttyUSB0"
+    baud_rate = 9600
+    node = ArduinoMotorNode(port, baud_rate)
+    node.run()
